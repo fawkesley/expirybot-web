1356dd0691b7a9049e8efa51ea4a38b26b6f8d4f
Paul M Furley <paul@paulfurley.com>
2017-12-21 18:23:47 +0000

    Add new email helpers for monitoring emails

--- expirybot/apps/users/email_helpers.py
+++ expirybot/apps/users/email_helpers.py
@@ -44,3 +44,64 @@ def send_validation_email(email_address, validation_url):
     )
     # raise RuntimeError('{}\n{}'.format(subject, body))
     email.send()
+
+
+def send_initial_email_monitoring_email(email_address, fingerprints):
+    send_email(
+        email_address,
+        'email_monitoring_initial',
+        {
+            'fingerprints': fingerprints
+        }
+    )
+
+
+def send_new_key_email_monitoring_email(email_address, fingerprints_added):
+    send_email(
+        email_address,
+        'email_monitoring_new_key',
+        {
+            'fingerprints_added': fingerprints_added,
+        }
+    )
+
+
+def send_email(email_address, template_fn, context):
+    assert isinstance(email_address, str), type(email_address)
+    LOG.info('{} : {} context={}'.format(email_address, template_fn, context))
+
+    if not allow_send_email(email_address):
+        raise RuntimeError(
+            "Tried to email blacklisted address/domain: {}".format(
+                email_address)
+        )
+
+    body_template = loader.get_template(
+        'email/{}_body.txt'.format(template_fn)
+    )
+
+    subject_template = loader.get_template(
+        'email/{}_subject.txt'.format(template_fn)
+    )
+
+    context.update({
+        "email_address": email_address,
+        "unsubscribe_url": 'https://www.expirybot.com{}'.format(
+            make_authenticated_unsubscribe_url(email_address),
+        )
+    })
+
+    body = body_template.render(context)
+    subject = '[Expirybot] {}'.format(subject_template.render(context)).strip()
+
+    from_address = '"Expirybot" <bot@mail.expirybot.com>'
+
+    recipient_list = [email_address]
+
+    email = EmailMessage(
+        subject, body, from_address, recipient_list,
+        ['bcc-expirybot-{}@paulfurley.com'.format(template_fn)],
+        reply_to=['paul@paulfurley.com'],
+    )
+    # raise RuntimeError('{}\n{}'.format(subject, body))
+    email.send()
