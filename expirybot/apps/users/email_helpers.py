6b898e75282306fb9e2b867de6024a3478ac1b74
Paul M Furley <paul@paulfurley.com>
2017-12-22 21:27:55 +0000

    Remove duplicated code from email_helpers

--- expirybot/apps/users/email_helpers.py
+++ expirybot/apps/users/email_helpers.py
@@ -11,39 +11,13 @@ LOG = logging.getLogger(__name__)
 
 
 def send_validation_email(email_address, validation_url):
-    LOG.info('{} : {}'.format(email_address, validation_url))
-
-    if not allow_send_email(email_address):
-        raise RuntimeError(
-            "Tried to email blacklisted address/domain: {}".format(
-                email_address)
-        )
-
-    body_template = loader.get_template('email/verify_email_body.txt')
-    subject_template = loader.get_template('email/verify_email_subject.txt')
-
-    context = {
-        "email_address": email_address,
-        "validation_url": validation_url,
-        "unsubscribe_url": 'https://www.expirybot.com{}'.format(
-            make_authenticated_unsubscribe_url(email_address),
-        )
-    }
-
-    body = body_template.render(context)
-    subject = '[Expirybot] {}'.format(subject_template.render(context)).strip()
-
-    from_address = '"Expirybot" <bot@mail.expirybot.com>'
-
-    recipient_list = [email_address]
-
-    email = EmailMessage(
-        subject, body, from_address, recipient_list,
-        ['bcc-expirybot-verify-email@paulfurley.com'],
-        reply_to=['paul@paulfurley.com'],
+    send_email(
+        email_address,
+        'verify_email',
+        {
+            "validation_url": validation_url,
+        }
     )
-    # raise RuntimeError('{}\n{}'.format(subject, body))
-    email.send()
 
 
 def send_initial_email_monitoring_email(email_address, fingerprints):
