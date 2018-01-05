1e59685bbce6ac74f86c347845eec13b0864f310
Paul M Furley <paul@paulfurley.com>
2018-01-05 17:26:49 +0000

    Factor out GetRedirectUrlMixin

--- expirybot/apps/users/views/sign_up_view.py
+++ expirybot/apps/users/views/sign_up_view.py
@@ -4,22 +4,19 @@ from django.conf import settings
 
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.views import (
-    SuccessURLAllowedHostsMixin,
-)
 
 from django.shortcuts import resolve_url
-from django.utils.http import is_safe_url
 
 from django.views.generic.edit import FormView
+
+from .mixins import GetRedirectUrlMixin
 
 LOG = logging.getLogger(__name__)
 
 
-class SignUpView(SuccessURLAllowedHostsMixin, FormView):
+class SignUpView(GetRedirectUrlMixin, FormView):
     form_class = UserCreationForm
     template_name = 'users/sign_up.html'
-    redirect_field_name = 'next'
 
     def form_valid(self, form):
         form.save()  # save the new user first
@@ -34,20 +31,6 @@ class SignUpView(SuccessURLAllowedHostsMixin, FormView):
     def get_success_url(self):
         url = self.get_redirect_url()
         return url or resolve_url(settings.LOGIN_REDIRECT_URL)
-
-    def get_redirect_url(self):
-        """Return the user-originating redirect URL if it's safe."""
-
-        redirect_to = self.request.POST.get(
-            self.redirect_field_name,
-            self.request.GET.get(self.redirect_field_name, '')
-        )
-        url_is_safe = is_safe_url(
-            url=redirect_to,
-            allowed_hosts=self.get_success_url_allowed_hosts(),
-            require_https=self.request.is_secure(),
-        )
-        return redirect_to if url_is_safe else ''
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
