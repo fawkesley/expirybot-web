51d94d3d9878827f8ca0117906d3f26a1ea37f46
Paul M Furley <paul@paulfurley.com>
2018-01-05 11:45:19 +0000

    TEMPORARILY redirect / to /monitor-email-address/

--- expirybot/urls.py
+++ expirybot/urls.py
@@ -19,9 +19,15 @@ from django.conf.urls.static import static
 from django.conf.urls import url, include
 from django.contrib import admin
 from django.views.generic import TemplateView
+from django.views.generic.base import RedirectView
 
 
 urlpatterns = [
+    url(
+        r'^$',
+        RedirectView.as_view(url='/monitor-email-address/', permanent=False)
+    ),
+
     url(r'^admin/', admin.site.urls),
 
     url(
