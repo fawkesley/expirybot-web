5abd30f00219b5b112318022857d23992f5df2d3
Paul M Furley <paul@paulfurley.com>
2018-01-05 18:37:38 +0000

    Add index page

--- expirybot/urls.py
+++ expirybot/urls.py
@@ -19,13 +19,12 @@ from django.conf.urls.static import static
 from django.conf.urls import url, include
 from django.contrib import admin
 from django.views.generic import TemplateView
-from django.views.generic.base import RedirectView
 
 
 urlpatterns = [
     url(
         r'^$',
-        RedirectView.as_view(url='/monitor-email-address/', permanent=False)
+        TemplateView.as_view(template_name='expirybot/index.html')
     ),
 
     url(r'^admin/', admin.site.urls),
