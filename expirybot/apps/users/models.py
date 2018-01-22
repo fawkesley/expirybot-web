36005c5b6769cdf6dc23f948f1324e2f2383a4f9
Paul M Furley <paul@paulfurley.com>
2018-01-22 15:53:40 +0000

    Show possible keys on settings page

--- expirybot/apps/users/models.py
+++ expirybot/apps/users/models.py
@@ -1,3 +1,4 @@
+import logging
 import uuid
 
 from django.db import models
@@ -7,6 +8,9 @@ from django.db.models.signals import post_save
 from django.contrib.postgres.fields import ArrayField
 
 from expirybot.apps.blacklist.models import EmailAddress
+from expirybot.apps.keys.helpers import get_key
+
+LOG = logging.getLogger(__name__)
 
 
 class UserProfile(models.Model):
@@ -49,6 +53,29 @@ class UserProfile(models.Model):
     @property
     def is_temporary(self):
         return self.user.username.startswith('tmp-')
+
+    @property
+    def possible_pgp_keys(self):
+        fingerprints = set()
+
+        for email in self.owned_email_addresses:
+            try:
+                search_results = SearchResultForKeysByEmail.objects.get(
+                    email_address=email
+                )
+            except SearchResultForKeysByEmail.DoesNotExist:
+                continue
+            else:
+                fingerprints.update(search_results.key_fingerprints)
+
+        keys = []
+        for f in sorted(fingerprints):
+            try:
+                keys.append(get_key(f))
+            except Exception as e:
+                LOG.error(e)
+
+        return keys
 
 
 def create_user_profile(sender, instance, created, **kwargs):
