e0a746910796fec46c27c43ecc875d0971bcb80d
Paul M Furley <paul@paulfurley.com>
2018-01-22 17:11:20 +0000

    Improve sync_keys command

--- expirybot/apps/keys/management/commands/sync_keys.py
+++ expirybot/apps/keys/management/commands/sync_keys.py
@@ -1,10 +1,13 @@
+import datetime
 import logging
 
 from django.core.management.base import BaseCommand
+from django.utils import timezone
 
-from expirybot.apps.keys.helpers import sync_key
-
+from expirybot.apps.keys.helpers import get_key, sync_key
 from expirybot.apps.keys.models import PGPKey
+
+from expirybot.apps.users.models import SearchResultForKeysByEmail
 
 LOG = logging.getLogger(__name__)
 
@@ -27,11 +30,50 @@ class Command(BaseCommand):
 
 
 def sync_keys():
-    for key in get_keys_never_synced():
+    new_fingerprints = get_new_fingerprints_from_search_results()
+    keys_never_synced = get_keys_never_synced()
+    stale_keys = get_stale_keys()
+
+    LOG.info("{} new fingerprints, {} keys never synced, {} stale keys".format(
+        len(new_fingerprints), len(keys_never_synced), len(stale_keys)))
+
+    for fingerprint in new_fingerprints:
+        get_key(fingerprint)
+
+    for key in keys_never_synced:
+        sync_key(key)
+
+    for key in stale_keys:
         sync_key(key)
 
     LOG.info("sync_keys finished.")
 
 
+def get_new_fingerprints_from_search_results():
+    """
+    Find key fingerprints from SearchResultForKeysByEmail which haven't yet
+    been added as PGPKey objects, and add them.
+    """
+
+    already_got_fingerprints = set(
+        p.fingerprint for p in PGPKey.objects.all()
+    )
+    found_fingerprints = set()
+
+    for search_result in SearchResultForKeysByEmail.objects.all():
+        found_fingerprints.update(search_result.key_fingerprints)
+
+    return found_fingerprints - already_got_fingerprints
+
+
 def get_keys_never_synced():
     return PGPKey.objects.filter(last_synced=None)
+
+
+def get_stale_keys():
+    one_day_ago = timezone.now() - datetime.timedelta(hours=24)
+
+    return PGPKey.objects.filter(
+        last_synced__isnull=False,
+        last_synced__lt=one_day_ago
+    ).order_by('last_synced')  # ascending: oldest first
