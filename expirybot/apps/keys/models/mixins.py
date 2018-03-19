e14e3bcd96d46146da5b9b13343ac11a0e2dcb2f
Paul M Furley <paul@paulfurley.com>
2018-03-19 16:56:40 +0000

    Factor out FingerprintFormatMixin

--- expirybot/apps/keys/models/mixins.py
+++ expirybot/apps/keys/models/mixins.py
@@ -21,3 +21,27 @@ class ExpiryCalculationMixin():
             raise ValueError('days_till_expiry called with expiry_date=None')
 
         return (self.expiry_date - timezone.now().date().today()).days
+
+
+class FingerprintFormatMixin():
+    @property
+    def human_fingerprint(self):
+        if len(self.fingerprint) == 40:
+            return '{} {} {} {} {}  {} {} {} {} {}'.format(  # nbsp
+                self.fingerprint[0:4],
+                self.fingerprint[4:8],
+                self.fingerprint[8:12],
+                self.fingerprint[12:16],
+                self.fingerprint[16:20],
+                self.fingerprint[20:24],
+                self.fingerprint[24:28],
+                self.fingerprint[28:32],
+                self.fingerprint[32:36],
+                self.fingerprint[36:40]
+            )
+        else:
+            return self.fingerprint
+
+    @property
+    def zero_x_fingerprint(self):
+        return '0x{}'.format(self.fingerprint)
